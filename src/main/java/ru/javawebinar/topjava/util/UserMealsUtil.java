91e655ef7dcfc795140067fd3c8bb3ce09dbe332
Sergey Solomevich <sergeysolomevich@gmail.com>
2017-03-14 23:36:56 +0300

    HW0

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -3,11 +3,9 @@ package ru.javawebinar.topjava.util;
 import ru.javawebinar.topjava.model.UserMeal;
 import ru.javawebinar.topjava.model.UserMealWithExceed;
 
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.Month;
-import java.util.Arrays;
-import java.util.List;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
 
 /**
  * GKislin
@@ -30,6 +28,36 @@ public class UserMealsUtil {
 
     public static List<UserMealWithExceed>  getFilteredWithExceeded(List<UserMeal> mealList, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
         // TODO return filtered list with correctly exceeded field
-        return null;
+
+        LocalDateTime date0 = mealList.get(0).getDateTime();
+        String asBasicIsoDate0 = date0.format(DateTimeFormatter.BASIC_ISO_DATE);
+        int cal=mealList.get(0).getCalories();
+
+        List<UserMealWithExceed> mealWithExceedList = new ArrayList<>();
+        Map<LocalDate, Integer> map = new HashMap<>();
+
+        for (int i=1;i<mealList.size();i++)
+        {
+            LocalDateTime date = mealList.get(i).getDateTime();
+            String asBasicIsoDate = date.format(DateTimeFormatter.BASIC_ISO_DATE);
+
+            if (asBasicIsoDate.equals(asBasicIsoDate0))
+            {
+                cal += mealList.get(i).getCalories();
+            }
+            else
+            {
+                cal=mealList.get(i).getCalories();
+                asBasicIsoDate0 = date.format(DateTimeFormatter.BASIC_ISO_DATE);
+            }
+            map.put(date.toLocalDate(),cal);
+        }
+        for (int i=0;i<mealList.size();i++)
+        {
+            if (mealList.get(i).getDateTime().toLocalTime().toSecondOfDay()>startTime.toSecondOfDay()&& mealList.get(i).getDateTime().toLocalTime().toSecondOfDay()<endTime.toSecondOfDay())
+            mealWithExceedList.add(new UserMealWithExceed(mealList.get(i).getDateTime(), mealList.get(i).getDescription(),mealList.get(i).getCalories(), map.get(mealList.get(i).getDateTime().toLocalDate())>caloriesPerDay)) ;
+        }
+
+        return mealWithExceedList;
     }
 }
